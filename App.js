377499155eef5383b300af770477240171248263
Chelsea Farley <chelsea@tripwiretech.com>
2022-05-24 17:55:59 +1200

    RN Fetch

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  let [isLoading, setIsLoading] = useState(true);
+  let [error, setError] = useState();
+  let [response, setResponse] = useState();
+
+  useEffect(() => {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoading(false);
+          setResponse(result);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      )
+  }, []);
+
+  const getContent = () => {
+    if (isLoading) {
+      return <ActivityIndicator size="large" />;
+    }
+
+    if (error) {
+      return <Text>{error}</Text>
+    }
+    
+    console.log(response);
+    return <Text>Bitcoin (USD): {response["bpi"]["USD"].rate}</Text>;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {getContent()}
       <StatusBar style="auto" />
     </View>
   );
